242a98e257fa9cbaf157312581b14c711428d9f8
takuyaw-w <22423740+takuyaw-w@users.noreply.github.com>
2022-02-12 21:41:31 +0900

    計算誤り修正

--- src/main.php
+++ src/main.php
@@ -17,72 +17,72 @@ function main(): void {
         if (is_numeric($stdin)) {
             $val = intval($stdin);
             array_push($stack, $val);
-        } else {
-            switch ($stdin) {
-                case '+':
-                    if (count($stack) < 2) {
-                        print_redln("データが存在しません。");
-                        break;
-                    }
-                    $a = array_pop($stack);
-                    $b = array_pop($stack);
-                    $c = $a + $b;
-                    array_push($stack, $c);
+            continue;
+        }
+        switch ($stdin) {
+            case '+':
+                if (count($stack) < 2) {
+                    print_redln("データが存在しません。");
                     break;
-                case '-':
-                    if (count($stack) < 2) {
-                        print_redln("データが存在しません。");
-                        break;
-                    }
-                    $a = array_pop($stack);
-                    $b = array_pop($stack);
-                    $c = $a - $b;
-                    array_push($stack, $c);
+                }
+                $b = array_pop($stack);
+                $a = array_pop($stack);
+                $c = $a + $b;
+                array_push($stack, $c);
+                break;
+            case '-':
+                if (count($stack) < 2) {
+                    print_redln("データが存在しません。");
                     break;
-                case '*':
-                    if (count($stack) < 2) {
-                        print_redln("データが存在しません。");
-                        break;
-                    }
-                    $a = array_pop($stack);
-                    $b = array_pop($stack);
-                    $c = $a * $b;
-                    array_push($stack, $c);
+                }
+                $b = array_pop($stack);
+                $a = array_pop($stack);
+                $c = $a - $b;
+                array_push($stack, $c);
+                break;
+            case '*':
+                if (count($stack) < 2) {
+                    print_redln("データが存在しません。");
                     break;
-                case '/':
-                    if (count($stack) < 2) {
-                        print_redln("データが存在しません。");
-                        break;
-                    }
-                    // 計算処理
-                    $a = array_pop($stack);
-                    $b = array_pop($stack);
-                    if ($b == 0) {
-                        print_redln("0で除算しようとしています。");
-                        $stack = [];
-                        break;
-                    }
-                    $c = $a / $b;
-                    array_push($stack, $c);
+                }
+                $b = array_pop($stack);
+                $a = array_pop($stack);
+                $c = $a * $b;
+                array_push($stack, $c);
+                break;
+            case '/':
+                if (count($stack) < 2) {
+                    print_redln("データが存在しません。");
                     break;
-                case '=':
-                    if (count($stack) !== 1) {
-                        print_redln("計算途中です。");
-                        break;
-                    }
-                    // 最終的な結果を出力
-                    $result = array_pop($stack);
-                    print_lightGreenln("計算結果： {$result}");
-                    break;
-                case '.':
-                    return;
-                case 'c':
+                }
+                // 計算処理
+                $b = array_pop($stack);
+                $a = array_pop($stack);
+                if ($b === 0) {
+                    print_redln("0で除算しようとしています。");
                     $stack = [];
                     break;
-                default:
-                    print_redln("不正な入力です。");
+                }
+                $c = $a / $b;
+                array_push($stack, $c);
+                break;
+            case '=':
+                if (count($stack) !== 1) {
+                    print_redln("計算途中です。");
                     break;
-            }
+                }
+                // 最終的な結果を出力
+                $result = array_pop($stack);
+                print_lightGreenln("計算結果： {$result}");
+                break;
+            case '.':
+                return;
+            case 'c':
+                $stack = [];
+                break;
+            default:
+                print_redln("不正な入力です。");
+                break;
         }
     }
 }
